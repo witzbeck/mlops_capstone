808eb06cda504a1b3f8258d378427a2cd8c06cb9
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2024-05-27 17:30:46 +0000

    chore: Update poetry lock files for app and frontend packages

--- scripts/poetry-lock-packages.sh
+++ scripts/poetry-lock-packages.sh
@@ -5,9 +5,8 @@ child_packages=("app" "frontend")
 
 # Script to lock only if pyproject.toml has changed
 for package in "${child_packages[@]}"; do
-    if echo "$@" | grep -q "$package/pyproject.toml"; then
-        echo "Updating lock file in $package"
-        (cd "$package" && poetry lock --no-update)
-        cd ..
-    fi
+    echo "Updating lock file in $package"
+    cd "$package"
+    poetry lock --no-update
+    cd ..
 done

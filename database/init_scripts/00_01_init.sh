50c6e58e783f569b2eb8c031ad95f4b26a464121
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2024-05-27 14:51:04 +0000

    rename database to app_db

--- database/init_scripts/00_01_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Connect to the default database 'postgres' and execute the commands
-psql -U postgres -d postgres -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
-psql -U postgres -d postgres -c "CREATE USER $MLFLOW_TRACKING_USER WITH PASSWORD '$MLFLOW_TRACKING_PASSWORD';"
-psql -U postgres -d postgres -c "CREATE DATABASE $MLFLOW_TRACKING_NAME;"
-psql -U postgres -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE $MLFLOW_TRACKING_NAME TO $MLFLOW_TRACKING_USER;"
-
-# Applying database migrations for MLflow
-mlflow db upgrade postgresql://$MLFLOW_TRACKING_USER:$MLFLOW_TRACKING_PASSWORD@$MLFLOW_TRACKING_HOST:$MLFLOW_TRACKING_PORT/$MLFLOW_TRACKING_NAME
--- database/init_scripts/01_01_create_databases.sql
+++
@@ -1,4 +0,0 @@
--- This script creates the necessary databases for the project
-
-    BEGIN;    CREATE DATABASE mlflowdb;         COMMIT;
-    BEGIN;    CREATE DATABASE docs;             COMMIT;
